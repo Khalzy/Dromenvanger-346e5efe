c6f47a72162990d2b918979477a7eccedfcd7e2d
Candace <2100978@talnet.nl>
2020-02-06 19:03:04 +0100

    Uitwerking van de exercise

---
+++ dromenvanger.php
@@ -0,0 +1,21 @@
+<?php $dreamcatcher = array();
+echo " Hoeveel vrienden zal ik vragen om hun droom?" . PHP_EOL;
+$amount = readline("");
+for ($i = 1; $i <= $amount; $i++) {
+    echo  "Wat is jouw naam?" . PHP_EOL;
+    $name = readline("");
+    echo "Hoeveel dromen ga je opgeven?" . PHP_EOL;
+    $amountD = readline("");
+    $catcher = array();
+    for ($f = 1; $f <= $amountD; $f++) {
+        echo "Wat is jouw droom?" . PHP_EOL;
+        $wdream = readline("");
+        array_push($catcher, $wdream);
+    }
+    $dreamcatcher[$name] = $catcher;
+}
+foreach ($dreamcatcher as $name => $wdream) {
+    foreach ($wdream as $catcher) {
+        echo "$name heeft als droom: $catcher" . PHP_EOL;
+    }
+}
